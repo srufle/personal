ca4ea7804e72f0393ffd2caf193a95292677bd7b
Stephen Rufle <srufle@gmail.com>
2019-04-29 21:31:19 -0700

    Adding items to create a word doc resume from markdown

---
+++ make_msdoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker pull jagregory/pandoc
+docker run -v $(pwd):/source jagregory/pandoc -o techs.docx -f html -t docx technologies.html
+docker run -v $(pwd):/source jagregory/pandoc -o output_github.docx -f markdown -t docx rufle_stephen_resume.md
+echo "Done"
