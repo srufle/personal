f2bb4085e245d48280bec76510fe206adbb5793b
Stephen Rufle <srufle@gmail.com>
2019-12-03 21:45:28 -0700

    Adding docx version of resume, plus scripts to create from Markdown

--- make_msdoc.sh
+++ make_msdoc.sh
@@ -3,3 +3,4 @@ docker pull jagregory/pandoc
 docker run -v $(pwd):/source jagregory/pandoc -o techs.docx -f html -t docx technologies.html
 docker run -v $(pwd):/source jagregory/pandoc -o output_github.docx -f markdown -t docx rufle_stephen_resume.md
 echo "Done"
+
